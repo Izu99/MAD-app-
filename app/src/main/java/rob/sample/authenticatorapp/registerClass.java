3782f9970b93f40d54228e917f7bf23ea89388b7
Isuru <isuruumanga123@gmail.com>
2020-09-24 22:41:38 +0530

    login , Register and feedback

---
+++ app/src/main/java/rob/sample/authenticatorapp/registerClass.java
@@ -0,0 +1,16 @@
+package rob.sample.authenticatorapp;
+
+public class registerClass {
+
+    public String cfullName,cEmail,cPhone;
+
+    public registerClass() {
+
+    }
+
+    public registerClass(String cfullName, String cEmail, String cPhone) {
+        this.cfullName = cfullName;
+        this.cEmail = cEmail;
+        this.cPhone = cPhone;
+    }
+}
